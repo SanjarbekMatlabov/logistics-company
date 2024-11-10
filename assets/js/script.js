67ab63297d2e9f082d08716c22f79ebf4bab5a0f
Sanjar Matlabov <153494157+sanjarbekmatlabov@users.noreply.github.com>
2024-11-10 23:21:42 +0500

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,4 +48,34 @@ fetch('navbar.html')
   .then(response => response.text())
   .then(html => {
     document.getElementById('form').innerHTML = html;
-  });
+  })
+  .then(html => {
+    const scriptURL = "https://script.google.com/macros/s/AKfycbwmdlk8UQS8Q79CDLNvrPFWl_YZZEZhAJm6-ttuoy6KfiEmFt8qErh1shIqcuBtz15r/exec";
+    const form = document.forms["submit-to-google-sheet"];
+    const submitButton = document.querySelector('input[type="submit"]'); // Submit tugmasini tanlash
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      // Submit tugmasini o'chirib qo'yish
+      submitButton.disabled = true; 
+      submitButton.style.cursor = "not-allowed"; 
+
+      const formData = new FormData(form);
+
+      fetch(scriptURL, { method: "POST", body: formData })
+        .then(response => {
+          swal("Done", "Submitted Successfully.", "success");
+          form.reset(); 
+        })
+        .catch(error => {
+          swal("Error", "Something went wrong. Please try again!", "error");
+          console.error("Error:", error);
+        })
+        .finally(() => {
+          // Submit tugmasini qayta yoqish
+          submitButton.disabled = false;
+          submitButton.style.cursor = "pointer"; 
+        });
+    });
+  })
